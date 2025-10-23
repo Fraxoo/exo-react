b51cdd4227fd4eb1f8fceebe42e9e686f8f08af9
Fraxoo <fraxo.officiel@gmail.com>
2025-10-23 09:48:04 +0200

    exo click

--- exo-react/src/App.jsx
+++ exo-react/src/App.jsx
@@ -1,7 +1,15 @@
+import { useState } from "react"
+
 export default function App(){
+
+    const [nbClick,setNbClick] = useState(0);
+    
+
+    
     return (
         <div>
-            <p>yo</p>
+            <p>{nbClick}</p>
+            <button onClick={()=> setNbClick(nbClick + 1)}>START</button>
         </div>
     )
 }
